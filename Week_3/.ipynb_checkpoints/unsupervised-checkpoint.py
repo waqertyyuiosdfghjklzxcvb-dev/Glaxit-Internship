714a3b28a0008f9737d8235b46213ecec685c7ab
Waleed Ahmed <waqertyyuiosdfghjklzxcvb@gmail.com>
2026-05-25 00:03:55 +0500

    Week 3

---
+++ Week_3/.ipynb_checkpoints/unsupervised-checkpoint.py
@@ -0,0 +1,20 @@
+# Un Supervised Learning
+# when we dont have a labels and outpout results and models learns to find the hidden patterns and grouoing the similar data points 
+# wihtout any human intervention
+
+# Clustering 
+# Grouping data in different clusters depends on the similarity of the data
+
+# Association Rules
+# It's a rule based learning means it finds the inetersting relationsship in the data and works on if-then condition 
+# Example : how much chances to buy milk when customer comes to buy for a egg.
+
+
+# Dimensionality Reduction
+# Reduce the dimensions means features and merges the most related columns to reduce complexity and simplify the visualization process
+
+
+
+# Anonmaly Detection
+# Model detects the unusal patterns means detceting the normal behaviour and if sees something wrong so detects the unusal data
+
